d272ba751f49070678e0b625de518ef1e269fd48
Mihir Shinde <mihirushinde29@gmail.com>
2021-10-07 22:39:08 +0530

    rectified the error

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,20 @@
 import { Box, Button, Typography } from "@mui/material"
+import { makeStyles } from '@mui/styles';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+  button: {
+      width: '100%',
+      margin: '10px 0'
+  },
+  link: {
+      textDecoration: 'none !important',
+      cursor: 'pointer'
+  }
+}))
 
 export const Home = () => {
+  const classes = useStyles()
   return (
     <Box height='100vh' display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
       <Box mb='50px'>
@@ -8,8 +22,12 @@ export const Home = () => {
       </Box>
       <Typography variant='h2' color='primary'>Continue with...</Typography>
       <Box display='flex' justifyContent='space-around' width='30%' mt='50px'>
-        <Button href='/login' color='primary' size='large' variant='contained' disableElevation>Log in</Button>
-        <Button href='/sign-up' color='secondary' size='large' variant='contained' disableElevation>Sign in</Button>
+        <Link to='/login' className={classes.link}>
+          <Button color='primary' size='large' variant='contained' disableElevation>Log in</Button>
+        </Link>
+        <Link to='/sign-up' className={classes.link}>
+          <Button color='secondary' size='large' variant='contained' disableElevation>Sign in</Button>
+        </Link>
       </Box>
     </Box>
   )
